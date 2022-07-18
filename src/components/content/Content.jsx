f1fd8688dffa669746831a2a6490d8d4303723c7
siihyun <sihyun930222@gmail.com>
2022-07-18 22:19:28 +0900

    <refactor> window.close 로직 제거

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,28 +1,21 @@
-import React from 'react';
-import styles from './content.module.css';
+import React from "react";
+import styles from "./content.module.css";
 
 const Content = () => {
   return (
     <section className={styles.contentSection}>
       <img
         className={styles.informationImage}
-        src='/images/img_section02.png'
-        alt='event information'
+        src="/images/img_section02.png"
+        alt="event information"
       ></img>
-      <div
-        onClick={() => {
-          window.opener = null;
-          window.open('', '_self');
-          window.close();
-        }}
+
+      <a
+        className={styles.linkPromotion}
+        href="https://link.yogig.com/tmcv21/AY2a"
       >
-        <a
-          className={styles.linkPromotion}
-          href='https://link.yogig.com/tmcv21/AY2a'
-        >
-          100만원 미션 찾으러 가긱
-        </a>
-      </div>
+        100만원 미션 찾으러 가긱
+      </a>
     </section>
   );
 };
--- src/components/fixedButton/FixedButton.jsx
+++ src/components/fixedButton/FixedButton.jsx
@@ -1,24 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import styles from './fixedButton.module.css';
+import styles from "./fixedButton.module.css";
 
 const FixedButton = () => {
   return (
-    <div
-      onClick={() => {
-        window.opener = null;
-        window.open('', '_self');
-        window.close();
-      }}
-    >
-      <a href='https://link.yogig.com/tmcv21/AY2a'>
-        <img
-          className={styles.fixedButton}
-          src='/images/img_float.png'
-          alt='event information'
-        ></img>
-      </a>
-    </div>
+    <a href="https://link.yogig.com/tmcv21/AY2a">
+      <img
+        className={styles.fixedButton}
+        src="/images/img_float.png"
+        alt="event information"
+      ></img>
+    </a>
   );
 };
 
